a113c35a910069b5ebd2090b3c8a935b7f912022
mirok16 <154271539+mirok16@users.noreply.github.com>
2026-02-15 14:04:55 +0300

    Add password validation logic

--- password_checker/checker.py
+++ password_checker/checker.py
@@ -2,3 +2,5 @@ def is_long_enough(password):
     return len(password) >= 8
 def has_digit(password):
     return any(char.isdigit() for char in password)
+def is_valid(password):
+    return is_long_enough(password) and has_digit(password)
